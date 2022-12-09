63865e4b6603a55a0b8a3573087d364c29f814dc
AndriyBidalov <120163615+AndriyBidalov@users.noreply.github.com>
2022-12-09 09:47:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
+search = "word1"
+replace = "word2"
+
+with open("text.txt", "r") as f:
+    data = f.read()
+    data = data.replace(search, replace)
+
+with open("text.txt", "w") as f:
+    f.write(data)
+
 open("text.txt")
 
 print("File text: ")
 print(open("text.txt").read())
-
-search = "word1"
-replaced = "word2"
-
-with open("text.txt", "r") as f:
-    data = f.read()
-    data = data.replace(search, replaced)
-
-with open("text.txt", "w") as f:
-    f.write(data)
 
 
 
